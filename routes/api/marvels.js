db667195fa0ec43ec24e7bb620e66f071382def6
alayssa13 <namiasalayssamae@gmail.com>
2024-03-11 15:27:28 +0800

    added client

--- routes/api/marvels.js
+++ routes/api/marvels.js
@@ -15,7 +15,7 @@ router.get('/', (req, res) => {
 });
 
 // @route   POST api/marvel
-// @desk    Create A Post 
+// @desk    Create An Item 
 // @access  Public
 router.post('/', (req, res) => {
   const newmarvel = new marvel({
@@ -23,6 +23,22 @@ router.post('/', (req, res) => {
   });
 
   newmarvel.save().then(marvels => res.json(marvels));
+});
+
+// @route   DELETE api/marvel/:id
+// @desk    Delete A Item 
+// @access  Public
+router.delete('/:id', (req, res) => {
+  marvel.findById(req.params.id)
+      .then(Marvel => {
+          if (!Marvel) {
+              return res.status(404).json({ success: false, message: 'Marvel not found' });
+          }
+          Marvel.deleteOne()
+              .then(() => res.json({ success: true }))
+              .catch(err => res.status(500).json({ success: false, error: err.message }));
+      })
+      .catch(err => res.status(500).json({ success: false, error: err.message }));
 });
 
 module.exports = router; 
